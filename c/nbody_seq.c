cb12a6657f3078b96664d5eb2a8603e811393ea6
Eric Stokes <letaris@gmail.com>
2019-10-28 17:50:58 -0400

    update sequential version of C nbody test

--- c/nbody_seq.c
+++ c/nbody_seq.c
@@ -1,134 +1,190 @@
 /* 
-yay faster than julia
+8000 bodies simulated for 10 minutes 100ms step granularity, about 8.83E12 fp ops.
 
-1000 bodies for 10 min
-real	0m45.394s
-user	0m45.346s
-sys	0m0.004s
+gcc 9.2
+Intel Core i7 7820X 8 cores 16 threads 4 GHz all core
+gcc -DNTHREADS=16 -O3 -ffast-math -march=skylake-avx512 -o nbody nbody.c -lpthread -lm
+real    0m48.137s
+user    12m24.197s
+sys     0m0.359s
+183.9 GFlops
 
-8000 bodies for 10 min
-real	52m28.486s
-user	52m25.458s
-sys	0m0.012s
+gcc 9.2
+IBM POWER9 8 cores 32 threads 3.8 GHz all core
+gcc -DNTHREADS=32 -O3 -ffast-math -mcpu=power9 -o nbody nbody.c -lpthread -lm
+real    2m56.109s (176s)
+user    91m47.160s
+sys     0m0.917s
+50.1 GFlops
+
+gcc 9.1
+Intel Core i7 8550U 4 cores 8 threads 2.5 GHz all core 4 GHz max
+gcc -DNTHREADS=8 -O3 -ffast-math -march=skylake -o nbody nbody.c -lpthread -lm
+real    2m59.049s (179s)
+user    21m42.140s
+sys     0m1.072s
+49.3 GFlops
+
+gcc 6.3
+Intel Core i7 4770K 4 cores 8 threads 3.8 GHz all core
+gcc -DNTHREADS=8 -O3 -ffast-math -march=haswell -o nbody nbody.c -lpthread -lm
+real    3m8.877s (189s)
+user    25m5.316s
+sys     0m0.308s
+46.7 GFlops
+
 */
 
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
+#include<pthread.h>
+#include<assert.h>
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+} vec3;
+
+int vec3_is_nan(vec3 *a) {
+  return fpclassify(a->x) == FP_NAN
+    || fpclassify(a->y) == FP_NAN
+    || fpclassify(a->z) == FP_NAN;
+}
+
+vec3 vec3_add(vec3 *a, vec3 *b) {
+  vec3 res = { .x = a->x + b->x,
+               .y = a->y + b->y,
+               .z = a->z + b->z };
+  return res;
+}
+
+void vec3_add_accum(vec3 *a, vec3 *b) {
+  a->x += b->x;
+  a->y += b->y;
+  a->z += b->z;
+}
+
+vec3 vec3_sub(vec3 *a, vec3 *b) {
+  vec3 res = { .x = a->x - b->x,
+               .y = a->y - b->y,
+               .z = a->z - b->z };
+  return res;
+}
+
+vec3 vec3_mul(vec3 *a, vec3 *b) {
+  vec3 res = { .x = a->x * b->x,
+               .y = a->y * b->y,
+               .z = a->z * b->z };
+  return res;
+}
+
+vec3 vec3_mul_scalar(vec3 *a, float b) {
+  vec3 res = { .x = a->x * b,
+               .y = a->y * b,
+               .z = a->z * b };
+  return res;
+}
+
+vec3 vec3_div(vec3 *a, vec3 *b) {
+  vec3 res = { .x = a->x / b->x,
+               .y = a->y / b->y,
+               .z = a->z / b->z };
+  return res;
+}
+
+float vec3_dotp(vec3 *a, vec3 *b) {
+  float r0 = a->x + b->x, r1 = a->y + b->y, r2 = a->z + b->z;
+  return r0 * r0 + r1 * r1 + r2 * r2;
+}
+
+typedef struct {
+  vec3* position;
+  vec3* velocity;
+  float * mass;
+  float step_duration;
+  int bodies;
+  int steps;
+} nb_ctxt;
 
 #define G 6.673e-11
-#define MOON_MASS 7.34e22
-#define MOON_RADIUS 1.7371e6
 
-void step(int n, float *position, float *velocity, float *mass, float duration) {
-  float r1, r2, r3, rsquared, accel, normal, dvx = 0, dvy = 0, dvz = 0;
+void compute_forces(nb_ctxt *ctx, int i, int j, vec3 *dv) {
+  vec3 r = vec3_sub(&ctx->position[j], &ctx->position[i]);
+  float rsquared = vec3_dotp(&r, &r);
+  float accel = (G * ctx->mass[i] * ctx->mass[j]) / rsquared;
+                                                                
+  /* compute the normal vector pointing from i to j */      
+  float normal = 1 / sqrtf(rsquared);
+    
+  /* now update the velocity */                             
+  vec3 dv_increment = vec3_mul_scalar(&r, normal * accel * ctx->step_duration);
+  vec3_add_accum(dv, &dv_increment);
+}
+
+void step(nb_ctxt *ctx) {
   int i, j;
 
   /* update velocities */
-  for(i = 0; i < n; i++) {
-    /* This is necessary in order to avoid branches inside the loop,
-       which will prevent the loop from vectorizing. And it's
-       necessary to skip computing an objects force on itself because
-       that will always be zero, resulting in NaN. */
-
-    #define LOOP \
-        /* compute the force of gravity */                       \
-        r1 = (position[j*3 + 0] - position[i*3 + 0]);            \
-        r2 = (position[j*3 + 1] - position[i*3 + 1]);            \
-        r3 = (position[j*3 + 2] - position[i*3 + 2]);            \
-        rsquared = r1*r1 + r2*r2 + r3*r3;                        \
-        accel = (G * mass[j]) / rsquared;                        \
-                                                                 \
-        /* compute the normal vector pointing from i to j */     \
-        normal = 1 / sqrtf(rsquared);                            \
-                                                                 \
-        /* now update the velocity */                            \
-        dvx += r1 * normal * accel * duration;                   \
-        dvy += r2 * normal * accel * duration;                   \
-        dvz += r3 * normal * accel * duration;                   \
-
-    for(j = 0; j < i; j++) {
-      LOOP
-    }
-    for(j = i + 1; j < n; j++) {
-      LOOP
-    }
-
-    velocity[i*3 + 0] += dvx;
-    velocity[i*3 + 1] += dvy;
-    velocity[i*3 + 2] += dvz;
+  for(i = 0; i < ctx->bodies; i+= 1) {
+    vec3 dv = {.x = 0, .y = 0, .z = 0};
+    for(j = 0; j < i; j++) compute_forces(ctx, i, j, &dv);
+    for(j = i + 1; j < ctx->bodies; j++) compute_forces(ctx, i, j, &dv);
+    vec3_add_accum(&ctx->velocity[i], &dv);
   }
 
   /* update positions */
-  for(i = 0; i < n; i++) {
-    position[i*3 + 0] += velocity[i*3 + 0] * duration;
-    position[i*3 + 1] += velocity[i*3 + 1] * duration;
-    position[i*3 + 2] += velocity[i*3 + 2] * duration;
-  }
-}
-
-/* This simulates a 1000 Kg body orbiting the moon at at distance of about 10 kilometers */
-void two_body_test() {
-  int i, n = 2;
-  float position[6] = 
-    { 0     , 0, 0, 
-      1.75e6, 0, 0 };
-  float velocity[6] = 
-    { 0, 0      , 0,
-      0, 1.673e3, 0 };
-  float mass[2] = {MOON_MASS, 1e3};
-  float alt = 0;
-
-  while(1) {
-    for(i = 0; i < 6000; i++) {
-      step(n, position, velocity, mass, 0.1);
-    }  
-
-    alt = sqrt(powf((position[0*3 + 0] - position[1*3 + 0]), 2) +
-               powf((position[0*3 + 1] - position[1*3 + 1]), 2) +
-               powf((position[0*3 + 2] - position[1*3 + 2]), 2));
-    printf("alt %f v %f,%f,%f\n", alt - MOON_RADIUS, 
-           velocity[3+0], velocity[3+1], velocity[3+2]);
+  for(i = 0; i < ctx->bodies; i += 1) {
+    vec3 dv = vec3_mul_scalar(&ctx->velocity[i], ctx->step_duration);
+    vec3_add_accum(&ctx->position[i], &dv);
   }
 }
 
 float random_float() {
-  float n = (float) rand();
-  return (n / (log10f(n) + 1)); /* (0, 1) */
+  return ((float) rand() / (float) RAND_MAX);
 }
 
-void many_body_test() {
-  int i, n = 1000;
-  float position[n*3];
-  float velocity[n*3];
-  float mass[n];
-  float alt;
-
-  srand(1232);
-  for(i = 0; i < n; i++) {
-    position[i*3 + 0] = random_float() * 1e9;
-    position[i*3 + 1] = random_float() * 1e9;
-    position[i*3 + 2] = random_float() * 1e9;
-
-    velocity[i*3 + 0] = random_float() * 5e2;
-    velocity[i*3 + 1] = random_float() * 5e2;
-    velocity[i*3 + 2] = random_float() * 5e2;
-
-    mass[i] = random_float() * 1e22;
-  }
-  
-  for(i = 0; i < 6000; i++) {
-    step(n, position, velocity, mass, 0.1);
-  }
-
-  alt = sqrt(powf((position[0*3 + 0] - position[1*3 + 0]), 2) +
-             powf((position[0*3 + 1] - position[1*3 + 1]), 2) +
-             powf((position[0*3 + 2] - position[1*3 + 2]), 2));
-
-  printf("alt %f v %f,%f,%f\n", alt, 
-         velocity[3+0], velocity[3+1], velocity[3+2]);
+static void * start_steps(nb_ctxt *ctx) {
+  for(int i = 0; i < ctx->steps; i++) step(ctx);
 }
+
+#define BODIES 8000
 
 void main() {
-  many_body_test();
+  int i;
+  vec3 position[BODIES];
+  vec3 velocity[BODIES];
+  float mass[BODIES];
+  nb_ctxt ctxt = { .position = position,
+                   .velocity = velocity,
+                   .mass = mass,
+                   .bodies = BODIES,
+                   .steps = 6000,
+                   .step_duration = 0.1 };
+  
+  /* init the universe with a stable random seed so we get a
+     repeatable benchmark */
+  srand(12345);
+  for(i = 0; i < BODIES; i++) {
+    position[i].x = random_float()* 1e8;
+    position[i].y = random_float()* 1e8;
+    position[i].z = random_float()* 1e8;
+
+    velocity[i].x = random_float()* 5e2;
+    velocity[i].y = random_float()* 5e2;
+    velocity[i].z = random_float()* 5e2;
+
+    mass[i] = random_float() * 1e12;
+  }
+  
+  start_steps(&ctxt);
+
+  /* Verify that nothing came out NaN, and print the results */
+  for(i = 0; i < BODIES; i++) {
+    vec3 p = position[i], v = velocity[i];
+    assert(!vec3_is_nan(&p));
+    assert(!vec3_is_nan(&v));
+    printf("p %g, %g, %g v %g,%g,%g\n", p.x, p.y, p.z, v.x, v.y, v.z);
+  }
 }
